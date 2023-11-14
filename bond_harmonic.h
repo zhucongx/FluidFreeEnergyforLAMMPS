9ee4b1caf8ee4229ad8627606a80651fce5974e3
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-11-14 18:22:35 -0500

    Update bond_harmonic.cpp/.h

--- bond_harmonic.h
+++ bond_harmonic.h
@@ -1,7 +1,7 @@
 /* -*- c++ -*- ----------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
-   http://lammps.sandia.gov, Sandia National Laboratories
-   Steve Plimpton, sjplimp@sandia.gov
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
 
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
@@ -16,48 +16,40 @@
    method by Rodolfo Paula Leite (Unicamp/BR).
 ------------------------------------------------------------------------- */
 
-#ifdef BOND_CLASS
-
-BondStyle(harmonic,BondHarmonic)
-
+#ifdef ANGLE_CLASS
+// clang-format off
+AngleStyle(harmonic,AngleHarmonic);
+// clang-format on
 #else
 
-#ifndef LMP_BOND_HARMONIC_H
-#define LMP_BOND_HARMONIC_H
+#ifndef LMP_ANGLE_HARMONIC_H
+#define LMP_ANGLE_HARMONIC_H
 
-#include <cstdio>
-#include "bond.h"
+#include "angle.h"
 
 namespace LAMMPS_NS {
 
-class BondHarmonic : public Bond {
+class AngleHarmonic : public Angle {
  public:
-  BondHarmonic(class LAMMPS *);
-  virtual ~BondHarmonic();
-  virtual void compute(int, int);
-  virtual void coeff(int, char **);
-  double equilibrium_distance(int);
-  void write_restart(FILE *);
-  virtual void read_restart(FILE *);
-  void write_data(FILE *);
-  double single(int, double, int, int, double &);
-  virtual void *extract(char *, int &);
+  AngleHarmonic(class LAMMPS *);
+  ~AngleHarmonic() override;
+  void compute(int, int) override;
+  void coeff(int, char **) override;
+  double equilibrium_angle(int) override;
+  void write_restart(FILE *) override;
+  void read_restart(FILE *) override;
+  void write_data(FILE *) override;
+  double single(int, int, int, int) override;
+  void born_matrix(int type, int i1, int i2, int i3, double &du, double &du2) override;
+  void *extract(const char *, int &) override;
 
  protected:
-  double *k,*r0;
+  double *k, *theta0;
   double *fscale;
   virtual void allocate();
 };
 
-}
+}    // namespace LAMMPS_NS
 
 #endif
 #endif
-
-/* ERROR/WARNING messages:
-
-E: Incorrect args for bond coefficients
-
-Self-explanatory.  Check the input script or data file.
-
-*/
